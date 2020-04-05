d38a4d8653f9c26e94248b65c57494e7bd67b8f8
Igor Mihalik <igor.mihalik@gmail.com>
2020-04-05 19:01:30 +0200

    v2 as alias to master /v2

--- sockjs/sockjs.go
+++ sockjs/sockjs.go
@@ -1,13 +1,30 @@
 package sockjs
 
-// Session represents a connection between server and client.
-type Session interface {
-	// Id returns a session id
-	ID() string
-	// Recv reads one text frame from session
-	Recv() (string, error)
-	// Send sends one text frame to session
-	Send(string) error
-	// Close closes the session with provided code and reason.
-	Close(status uint32, reason string) error
+import (
+	"net/http"
+
+	"github.com/i9m/sockjs-go/v2/sockjs"
+)
+
+type Session = sockjs.Session
+type Options = sockjs.Options
+type Handler = sockjs.Handler
+type SessionState = sockjs.SessionState
+
+const (
+	SessionOpening = sockjs.SessionOpening
+	SessionActive  = sockjs.SessionActive
+	SessionClosing = sockjs.SessionClosing
+	SessionClosed  = sockjs.SessionClosed
+)
+
+var DefaultOptions = sockjs.DefaultOptions
+var ErrErrSessionNotOpen = sockjs.ErrSessionNotOpen
+
+func NewHandler(prefix string, opts Options, handlerFunc func(Session)) *Handler {
+	return sockjs.NewHandler(prefix, opts, handlerFunc)
+}
+
+func DefaultJSessionID(rw http.ResponseWriter, req *http.Request) {
+	sockjs.DefaultJSessionID(rw, req)
 }
